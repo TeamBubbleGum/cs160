03dbcbfb6d949050dc79bc0e68cc26d226e6574e
Cagan Sevencan <cagansvncn@gmail.com>
2020-03-21 16:24:34 -0700

    Try

--- Unload/AppDelegate.swift
+++ Unload/AppDelegate.swift
@@ -5,13 +5,12 @@
 //  Created by Cagan Sevencan on 3/4/20.
 //  Copyright © 2020 Cagan Sevencan. All rights reserved.
 //
-
 import UIKit
 import Parse
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+ 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -22,11 +21,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                    })
                )
         
+        
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -41,4 +41,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
--- Unload/LoginViewController.swift
+++ Unload/LoginViewController.swift
@@ -17,43 +17,94 @@ class LoginViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: Private Methods
+    private func showErrorAlert(with title: String, message: String){
+           let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                // handle response here.
+            }
+            // add the OK action to the alert controller
+            alertController.addAction(OKAction)
+            
+            present(alertController, animated: true)
+    }
+    
+    
     //We want to segue conditonally after the network confirms
     @IBAction func onSignIn(_ sender: Any) {
-        let username = usernameField.text!
-        let password = passwordField.text!
+        guard let username = usernameField.text,
+            let password = passwordField.text else{
+                return
+        }
+        
+        let errorTitle = "Cannot Sign In"
+        if username.isEmpty || password.isEmpty{
+         
+            let message = "Please fill out all fields"
+            showErrorAlert(with: errorTitle, message: message)
+        }
+        if username.contains(" ") || password.contains(" "){
+                   let message = "Username/Password cannot contain spaces"
+                   showErrorAlert(with: errorTitle, message: message)
+               }
         
         PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            
             if user != nil {   //if we make it inside this function meaning we have a user otherwise its null
+               let vc = FeedViewController()
+               vc.modalPresentationStyle = .fullScreen
                 self.performSegue(withIdentifier: "loginSegue", sender: nil)
                 print("Succesfully signed in!")
             }else{
-                print("Error: \(error?.localizedDescription)")
+                let errorMessage = error?.localizedDescription ?? "There was a problem signing in."
+                self.showErrorAlert(with: errorTitle, message: errorMessage)
+                
             }
         }
     }
     
     @IBAction func onSignUp(_ sender: Any) {
+        guard let username = usernameField.text,
+            let password = passwordField.text else{
+                return
+        }
+        let errorTitle = "Cannot Sign Up"
+        
+        if username.isEmpty || password.isEmpty{
+                let message = "Please fill out all fields"
+                 showErrorAlert(with: errorTitle, message: message)
+        }
+        if username.contains(" ") || password.contains(" "){
+            let message = "Username/Password cannot contain spaces"
+            showErrorAlert(with: errorTitle, message: message)
+        }
+        
         let user = PFUser()       //changing to a constant using let, since we are not changing the user
-        user.username = usernameField.text
-        user.password = passwordField.text
+        user.username = username
+        user.password = password
         //user.email = "email@example.com"
         // other fields can be set just like with PFObject
        // user["phone"] = "415-392-0202"
          
         //if it is sucessful go to navigation pane
         user.signUpInBackground { (success, error) in
+            let vc = FeedViewController()
+            vc.modalPresentationStyle = .fullScreen
             if success{
                 self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                
                 print("Succesfully signedup!")
             }else{
-                print("Error: \(error?.localizedDescription)")
+                let errorMessage = error?.localizedDescription ?? "There was a problem signing up."
+                self.showErrorAlert(with: errorTitle, message: errorMessage)
             }
         }
     }
     
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
