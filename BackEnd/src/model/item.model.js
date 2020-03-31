2d4ed9fd43faeb216f706771224a799e1bea8803
josh-gill <joshx555@comcast.net>
2020-03-31 15:01:36 -0700

    Item Route and Test Routes

--- BackEnd/src/model/item.model.js
+++ BackEnd/src/model/item.model.js
@@ -4,11 +4,12 @@ let itemSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectID,
     name:{type: String, required: true},
     description: {type: String},
-    weight: {type: String,match: /([\d.]+)\s+(lbs?|oz|g|kg) /},
+    weight: {type: String, match: /([\d.]+)\s+(lbs?|oz|g|kg) /},
     dimensions: {type: String, match: /(\d+(?:,\d+)?) x (\d+(?:,\d+)?)(?: x (\d+(?:,\d+)?))?/},
-    seller: {type: String, require: true},
+    seller: {type: mongoose.Schema.Types.ObjectID}, //require seller's ID
     tag: [String], //array of strings for tags
-    zip: {type: String, require: true, match: /^(\d{5}(?:\-\d{4})?)$/}
+    zip: {type: String, match: /^(\d{5}(?:\-\d{4})?)$/}
 })
 
 module.exports = mongoose.model('Item', itemSchema)
+//require: true
